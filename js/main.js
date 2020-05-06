75658c408c04c74621e615fe46b4bf759bfaa8c2
Alizee Jan <jan.alizee@gmail.com>
2020-05-06 17:56:39 +0200

    navigation test

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ console.log("Bienvenue !");
 $(document).ready(function() {
   $('#container').fullpage({
     navigation: true,
-    slidesNavigation: true,
+    slidesNavigation: false,
     controlArrows: true,
     anchors:['home','work','contact']
   });
